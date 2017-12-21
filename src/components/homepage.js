ced310b7e92169e9c572e50fe67ea075aa84a394
jauxsh <joshua.jih.chang@gmail.com>
2017-12-20 21:55:45 -0800

    finished homepage

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Header from './header';
 import Footer from './footer';
 import BannerImg from './bannerImg';
-import homeImg from '../assets/images/welcome-image.png';
 import HomeContent from './homeContent';
 import SampleMenu from './sampleMenu'
+
+import homeImg from '../assets/images/welcome-image.png';
+
 
 const Homepage = () => {
     return (
