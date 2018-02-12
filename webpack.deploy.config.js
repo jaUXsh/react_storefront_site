0f4180fef3f8c37b21629dac122a7007daf1aa8a
jauxsh <joshua.jih.chang@gmail.com>
2018-02-12 15:57:15 -0800

    edit webpack.deploy.config for relative paths for assets

--- webpack.deploy.config.js
+++ webpack.deploy.config.js
@@ -22,7 +22,7 @@ module.exports = {
             },
             {
                 test: /\.(gif|png|jpe?g|svg)$/i,
-                use: ['file-loader?hash=sha512&digest=hex&name=imgs/[hash].[ext]',
+                use: ['file-loader?hash=sha512&digest=hex&name=./imgs/[hash].[ext]',
                     'image-webpack-loader?bypassOnDebug']
             }
         ]
